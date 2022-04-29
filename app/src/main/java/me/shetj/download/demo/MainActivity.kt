0bc6d52fc31c2fa3f400a22c6020632c50876d44
Jun <375105540@qq.com>
2022-04-29 10:23:20 +0800

    增加案例：监听WorkManger下载进度

--- app/src/main/java/me/shetj/download/demo/MainActivity.kt
+++ app/src/main/java/me/shetj/download/demo/MainActivity.kt
@@ -1,12 +1,16 @@
 package me.shetj.download.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import androidx.work.Data
+import androidx.work.WorkInfo.State.FAILED
+import androidx.work.WorkInfo.State.RUNNING
+import androidx.work.WorkInfo.State.SUCCEEDED
+import androidx.work.WorkManager
+import java.util.*
 import kotlinx.coroutines.launch
 import me.shetj.download.http.KCHttpV2
 import me.shetj.download.worker.DownloadWorker
@@ -24,26 +28,53 @@ class MainActivity : AppCompatActivity() {
         val tv_msg = findViewById<TextView>(R.id.tv_msg)
 
         findViewById<View>(R.id.test_DownloadWorker).setOnClickListener {
-            DownloadWorker.startDownload(
+            val startDownload = DownloadWorker.startDownload(
                 this@MainActivity,
                 downloadUrl,
                 this@MainActivity.cacheDir.path,
                 "wxwork_android_3.apk"
             )
+            onWorkDownProcess(tv_msg, startDownload)
         }
 
         findViewById<View>(R.id.test_Download).setOnClickListener {
             tv_msg.text = ""
-            lifecycleScope.launch{
-                KCHttpV2.download(downloadUrl, "${this@MainActivity.cacheDir.path}/${"wxwork_android_3.apk"}", onProcess = { _, _, process ->
-                    Log.i("download","process =${(process * 100).toInt()}% \n")
-                    tv_msg.append("process =${(process * 100).toInt()}% \n")
-                }, onSuccess = {
-                   Log.i("download",filesDir.path)
-                },onError = {
-                    it.printStackTrace()
-                })
+            lifecycleScope.launch {
+                KCHttpV2.download(
+                    downloadUrl,
+                    "${this@MainActivity.cacheDir.path}/${"wxwork_android_3.apk"}",
+                    onProcess = { _, _, process ->
+                        Log.i("download", "process =${(process * 100).toInt()}% \n")
+                        tv_msg.append("下载进度：progress =${(process * 100).toInt()}% \n")
+                    },
+                    onSuccess = {
+                        tv_msg.append("下载成功\n")
+                        Log.i("download", filesDir.path)
+                    },
+                    onError = {
+                        it.printStackTrace()
+                        tv_msg.append("下载失败\n")
+                    })
             }
         }
+    }
+
+    private fun onWorkDownProcess(tv_msg: TextView, startDownload: UUID) {
+        tv_msg.text = ""
+        WorkManager.getInstance(applicationContext)
+            .getWorkInfoByIdLiveData(startDownload)
+            .observe(this) { t -> // 任务执行完毕之后，会在这里获取到返回的结果
+                when (t?.state) {
+                    RUNNING -> {
+                        tv_msg.append("下载进度：progress =${t.progress.getInt("progress", 0)}% \n")
+                    }
+                    SUCCEEDED -> {
+                        tv_msg.append("下载成功\n")
+                    }
+                    FAILED -> {
+                        tv_msg.append("下载失败\n")
+                    }
+                }
+            }
     }
 }
